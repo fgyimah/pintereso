9f55b116337b24cc07a18b54c8c1a91bb0abada7
Francis Gyimah <fgyimah883@gmail.com>
2019-07-18 16:49:17 +0000

    created a rails app with webpack support, bootstrap and jquery

---
+++ pintereso/config/initializers/content_security_policy.rb
@@ -0,0 +1,23 @@
+Rails.application.config.content_security_policy do |p|
+  p.font_src    :self, :https, :data
+  p.img_src     :self, :https, :data
+  p.object_src  :none
+  p.style_src   :self, :https, :unsafe_inline
+
+
+  p.script_src :self, :https
+  p.default_src :self, :https
+
+  if Rails.env.development?
+    p.connect_src :self, :https, 'ws://localhost:3000'
+  end
+
+  # Specify URI for violation reports
+  # p.report_uri "/csp-violation-report-endpoint"
+
+  # standard until this moment
+  if Rails.env.development?
+    p.script_src :self, :https, :unsafe_eval
+    p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
+  end
+end
